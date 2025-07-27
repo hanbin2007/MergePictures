47e019bca66783f12979ea6bce5ee80dfd041867
ZhangHanbin <thebestzhq@gmail.com>
2025-07-27 17:39:15 +0800

    Initial Commit

---
+++ MergePictures/MergePicturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MergePicturesApp.swift
+//  MergePictures
+//
+//  Created by zhb on 2025/7/27.
+//
+
+import SwiftUI
+
+@main
+struct MergePicturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
